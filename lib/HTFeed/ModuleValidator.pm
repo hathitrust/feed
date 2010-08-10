837b67903710c916be8a60b8130404bca447229d
Ryan Rotter <rrotter@umich.edu>
2010-08-10 10:29:22 -0400

    added stubs for ModuleValidator, QueryLib, and their respective JPEG2000-hul subclasses

---
+++ lib/HTFeed/ModuleValidator.pm
@@ -0,0 +1,32 @@
+package Feed::ModuleValidator;
+
+=info
+	ModuleValidator.pm
+	
+	prototype for Feed plugins
+	a plugin is responsible for validating Jhove output for **one Jhove module** as well as runnging any external filetype specific validation
+
+	For general Jhove output processing see Feed::Validator
+=cut
+
+
+use strict;
+use warnings;
+
+sub new{
+	
+}
+
+sub validate{
+	
+}
+
+sub _open_contexts{
+	
+}
+
+
+
+1;
+
+__END__;
