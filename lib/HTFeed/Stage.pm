bbc57bbf991945402576647f40fcddcd8922c37b
Ryan Rotter <rrotter@umich.edu>
2010-09-07 15:33:59 -0400

    More complete stage interface started VolumeValidator

--- lib/HTFeed/Stage.pm
+++ lib/HTFeed/Stage.pm
@@ -1,38 +1,68 @@
 package HTFeed::Stage;
 
+# abstract class, children must impliment following methods:
+# run
+# failed
+
 use warnings;
 use strict;
 use Carp;
+use Log::Log4perl qw(get_logger);
 
 use base qw(HTFeed::SuccessOrFailure);
 
 sub new{
-    my $class = shift;
+	my $class = shift;
 
-    # make sure we are instantiating a child class, not self
+	# make sure we are instantiating a child class, not self
 	if ($class eq __PACKAGE__){
-	    croak __PACKAGE__ . " can only construct subclass objects";
+		croak __PACKAGE__ . ' can only construct subclass objects';
 	}
-	    
-    my $self = {
-        volume  => undef,
-        @_,
-        has_run => 0,
-        failed  => 0,
-    };
-    
-    unless ($self->{volume} && ref($self->{volume}) eq "HTFeed::Volume"){
+		
+	my $self = {
+		volume  => undef,
+		@_,
+		has_run => 0,
+		failed  => 0,
+	};
+	
+	unless ($self->{volume} && $self->{volume}->isa("HTFeed::Volume")){
 		croak "$class cannot be constructed without an HTFeed::Volume";
 	}
 
-    bless ($self, $class);
-    return $self;
+	bless ($self, $class);
+	return $self;
 }
 
 # do somthing
 # abstract
 sub run{
-    croak "this is an abstract method";
+	croak 'this is an abstract method';
+}
+
+sub _set_done{
+	my $self = shift;
+	$self->{has_run}++;
+}
+
+sub failed{
+	my $self = shift;
+	return $self->{failed} if $self->{done};
+	croak "can't check success until run()";
+}
+
+sub _set_error{
+	# set fail, log errors
+	sub _set_error{
+		my $self = shift;
+		$self->{failed}++;
+
+		# log error w/ l4p
+		my $logger = get_logger(ref($self));
+		for (@_){
+				$logger->error($_,$self->{volume}->get_objid());
+		}
+	}
 }
 
 1;
--- lib/HTFeed/VolumeValidator.pm
+++ lib/HTFeed/VolumeValidator.pm
@@ -2,9 +2,79 @@ package HTFeed::VolumeValidator;
 
 use warnings;
 use strict;
-use Log::Log4perl;
+use Log::Log4perl qw(get_logger);
+use XML::LibXML;
+use HTFeed::ModuleValidator;
 
+use base qw(HTFeed::Stage);
 
+our $logger = get_logger(__PACKAGE__);
+
+sub run{
+	my $self = shift;
+	
+	my $volume $self->{volume};
+	
+	my @files = $volume->get_all_files;
+	my $dir = $volume->get_path;
+	my $volume_id = $volume->get_objid;
+	
+	$jhove_xml = _run_jhove($dir);
+	
+	# get xpc
+	my $jhove_xpc;
+	{
+		my $jhove_parser = XML::LibXML->new();
+		my $jhove_doc = $jhove_parser->parse_string($jhove_xml);
+		$jhove_xpc = new XML::LibXML::XPathContext($jhove_doc);
+	} 
+	
+	# get repInfo nodes
+	my $nodelist = $jhove_xpc->findnodes("//jhove:repInfo");
+
+	# put repInfo nodes in a usable data structure
+	my %nodes = ();
+	while (my $node = $nodelist->shift()){
+		# get uri for this node
+		my $fname = $jhove_xpc->findvalue("@uri",$node);
+		# trim
+		$fname =~ /(\/[0-9a-zA-Z.]*$)/;
+		$fname = $1;
+		
+		# populate hash
+		%nodes{$fname} = $node;
+	}
+	
+	for my $file (@files){
+		my $node;
+		
+		# get node for file
+		if($node = %nodes->{file}){
+			# run module validator
+			my $mod_val = HTFeed::ModuleValidator->new();
+			$mod_val->run();
+			# check, log success
+			if ($mod_val->succeeded()){
+				$logger->debug("$file in $volume_id ok");
+			}
+			else{
+				_set_error("$file bad");
+			}
+		}
+		else _set_error("$file missing");
+	}
+	
+	# do this last
+	$self->_set_done();
+}
+
+# run jhove
+sub _run_jhove{
+	my $dir = shift;
+	my $xml = `jhove -h XML -c /l/local/jhove-1.5/conf/jhove.conf $dir`;
+
+	return $xml;
+}
 
 
 1;
