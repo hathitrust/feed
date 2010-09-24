74a9320a32a6fb809a1f6ffcae58d72b7d7e3741
Ryan Rotter <rrotter@umich.edu>
2010-09-24 16:21:42 -0400

    HTFeed::Log::Warp::toDBArray

--- lib/HTFeed/Log/Warp.pm
+++ lib/HTFeed/Log/Warp.pm
@@ -3,8 +3,19 @@ package HTFeed::Log::Warp;
 use warnings;
 use strict;
 
-sub toDBArray{
-    die "not implimented";
+sub toDBArray {
+    # only do the transformation if we have more than one field
+    # otherwise it shouldn't be needed
+    if($#_){
+        my $error_message = HTFeed::Log->error_code_to_string(shift);
+        my %message_fields = @_;
+
+        my $error_array = HTFeed::Log->fields_hash_to_array(\%message_fields);
+    
+        return [$error_message, @$error_array];
+    }
+
+    return @_;
 }
 
 1;
