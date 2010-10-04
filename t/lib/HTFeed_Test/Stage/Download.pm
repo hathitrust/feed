599d59072aca67446f2214e07f2dd9efa7ac605e
Ryan Rotter <rrotter@umich.edu>
2010-10-04 12:30:24 -0400

    began unit test implimentation

---
+++ t/lib/HTFeed_Test/Stage/Download.pm
@@ -0,0 +1,20 @@
+package HTFeed_Test::Stage::Download;
+
+use warnings;
+use strict;
+
+use base qw(HTFeed::Test::Stage);
+
+use Test::More;
+
+#sub run_succeed Test(){
+#    
+#}
+
+#sub run_fail Test(){
+#    
+#}
+
+1;
+
+__END__
