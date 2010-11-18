b4968f3222bfe04c0cffa8c70441cbe906c87059
Ryan Rotter <rrotter@umich.edu>
2010-11-18 11:09:11 -0500

    added operation column handling for logging Update your db table accordingly (see table creation sql in config.l4p)

--- lib/HTFeed/Log/Warp.pm
+++ lib/HTFeed/Log/Warp.pm
@@ -6,10 +6,10 @@ use strict;
 # Synopsis
 # $logger->("ErrorCode",field => "data",...)
 sub toDBArray {
-    my $error_message = HTFeed::Log->error_code_to_string(shift);
+    my $error_message = HTFeed::Log::error_code_to_string(shift);
     my %message_fields = @_;
 
-    my $error_array = HTFeed::Log->fields_hash_to_array(\%message_fields);
+    my $error_array = HTFeed::Log::fields_hash_to_array(\%message_fields);
 
     return ($error_message, @$error_array);
 }
