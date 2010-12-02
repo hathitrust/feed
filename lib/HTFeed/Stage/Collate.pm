85f2ead95a3bba9c89dd3521ab874669e99a80f7
Aaron Elkiss <aelkiss@umich.edu>
2010-12-02 15:31:37 -0500

    Make symlinking optional for collate to support ingest in dev environment

--- lib/HTFeed/Stage/Collate.pm
+++ lib/HTFeed/Stage/Collate.pm
@@ -17,44 +17,52 @@ sub run{
     my $objid = $volume->get_objid();
     my $pairtree_objid = s2ppchars($objid);
     my $pairtree_object_path = sprintf('%s/%s/%s%s',get_config('repository'=>'destdir'),$namespace,id2ppath($pairtree_objid),$pairtree_objid);
-    my $pairtree_link_parent = sprintf('%s/%s/%s',get_config('repository'=>'authdir'),$namespace,id2ppath($pairtree_objid));
-    my $pairtree_link_path = $pairtree_link_parent . $pairtree_objid;
-    my $staging_dir = get_config('staging'=>'memory');
 
-    # this is a re-ingest if the dir already exists, log this
-    if (-d $pairtree_link_parent){
-        $self->set_info('Collating volume that is already in repo');
-        # make sure we have a link
-        unless (-l $pairtree_link_path){
-            $self->set_error('OperationFailed', detail => "link $pairtree_link_path missing, cannot reingest")
-                and return;
-        }
-    }
-    # make dir or error and return
-    else{
-        # make object path
-        make_path($pairtree_object_path)
-            or $self->set_error('OperationFailed', detail => "Could not create dir $pairtree_object_path") and return;
-        # make link path
-        make_path($pairtree_link_parent)
-            or $self->set_error('OperationFailed', detail => "Could not create dir $pairtree_link_path") and return;
-        # make link
-        symlink ($pairtree_object_path, $pairtree_link_path)
-            or $self->set_error('OperationFailed', detail => "Could not create dir $pairtree_link_path") and return;
+    # Create link from 'authdir' area, if needed
+
+    if(get_config('repository'=>'use_authdir')) {
+	my $pairtree_link_parent = sprintf('%s/%s/%s',get_config('repository','authdir'),$namespace,id2ppath($pairtree_objid));
+	my $pairtree_link_path = $pairtree_link_parent . $pairtree_objid;
+
+	# this is a re-ingest if the dir already exists, log this
+	if (-d $pairtree_link_parent){
+	    $self->set_info('Collating volume that is already in repo');
+	    # make sure we have a link
+	    unless (-l $pairtree_link_path){
+		$self->set_error('OperationFailed', detail => "link $pairtree_link_path missing, cannot reingest")
+		    and return;
+	    }
+	}
+	# make dir or error and return
+	else{
+	    # make object path
+	    make_path($pairtree_object_path)
+		or $self->set_error('OperationFailed', detail => "Could not create dir $pairtree_object_path") and return;
+	    # make link path
+	    make_path($pairtree_link_parent)
+		or $self->set_error('OperationFailed', detail => "Could not create dir $pairtree_link_path") and return;
+	    # make link
+	    symlink ($pairtree_object_path, $pairtree_link_path)
+		or $self->set_error('OperationFailed', detail => "Could not create dir $pairtree_link_path") and return;
+	}
+    } elsif(! -d $pairtree_object_path) {
+	make_path($pairtree_object_path)
+	    or $self->set_error('OperationFailed', detail => "Could not create dir $pairtree_object_path") and return;
     }
     
+    my $staging_dir = get_config('staging'=>'memory');
     my $mets_source = sprintf("%s/%s.mets.xml",$staging_dir,$objid);
     my $mets_target = sprintf("%s/%s.mets.xml",$pairtree_object_path,$pairtree_objid);
     my $zip_source = sprintf("%s/%s.zip",$staging_dir,$objid);
     my $zip_target = sprintf("%s/%s.zip",$pairtree_object_path,$pairtree_objid);
 
     # make sure the operation will succeed
-    if (-f $mets_source and -f $zip_source and -d $mets_target and -d $zip_target){
+    if (-f $mets_source and -f $zip_source and -d $pairtree_object_path){
         # move mets and zip to repo
-        copy($mets_source,$mets_target)
+        copy($mets_source,$pairtree_object_path)
             or $self->set_error('OperationFailed', detail => "cp $mets_source $mets_target failed: $!");
             
-        copy($zip_source,$zip_target)
+        copy($zip_source,$pairtree_object_path)
             or $self->set_error('OperationFailed', detail => "cp $zip_source $zip_target failed: $!");
 
         $self->_set_done();
