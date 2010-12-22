6b33780c144123a8a509902cd75847c3c4a00b18
Aaron Elkiss <aelkiss@umich.edu>
2010-12-22 13:09:27 -0500

    Migrate package type definitions for MDL to format required by cron_feed

--- lib/HTFeed/PackageType/MDLContoneComposite.pm
+++ lib/HTFeed/PackageType/MDLContoneComposite.pm
@@ -1,6 +1,7 @@
 package HTFeed::PackageType::MDLContoneComposite;
 use HTFeed::PackageType;
 use HTFeed::VolumeValidator;
+use HTFeed::PackageType::MDLContone::Unpack;
 use HTFeed::PackageType::MDLContoneComposite::METS;
 use HTFeed::Stage::Handle;
 use HTFeed::Stage::Pack;
@@ -12,14 +13,14 @@ our $identifier = 'mdlcontone_composite';
 
 our $config = {
     %{$HTFeed::PackageType::MDLContone::config},
-    
+
     # Regular expression that distinguishes valid files in the file package
     # HTML OCR is valid for the package type but only expected/required for UC1
     valid_file_pattern => qr/^( 
-		\w{3}\d{5}\w?\.(jp2|tif) |
-		mdl\.\w+\.\w{3}\d{5}\w?-all\.(xml) |
-		\w{3}\d{5}\w?\.(txt)
-		$)/x,
+    \w{3}\d{5}\w?\.(jp2|tif) |
+    mdl\.\w+\.\w{3}\d{5}\w?-all\.(xml) |
+    \w{3}\d{5}\w?\.(txt)
+    $)/x,
 
     # Configuration for each filegroup. 
     # prefix: the prefix to use on file IDs in the METS for files in this filegruop
@@ -30,24 +31,24 @@ our $config = {
     # jhove: set to 1 if output from JHOVE will be used in validation
     # utf8: set to 1 if files should be verified to be valid UTF-8
     filegroups => {
-	image => { 
-	    prefix => 'IMG',
-	    use => 'image',
-	    file_pattern => qr/\.(jp2|tif)$/,
-	    required => 1,
-	    content => 1,
-	    jhove => 1,
-	    utf8 => 0
-	},
-	ocr => { 
-	    prefix => 'OCR',
-	    use => 'ocr',
-	    file_pattern => qr/\.(txt)$/,
-	    required => 0,
-	    content => 1,
-	    jhove => 0,
-	    utf8 => 1
-	},
+        image => { 
+            prefix => 'IMG',
+            use => 'image',
+            file_pattern => qr/\.(jp2|tif)$/,
+            required => 1,
+            content => 1,
+            jhove => 1,
+            utf8 => 0
+        },
+        ocr => { 
+            prefix => 'OCR',
+            use => 'ocr',
+            file_pattern => qr/\.(txt)$/,
+            required => 0,
+            content => 1,
+            jhove => 0,
+            utf8 => 1
+        },
     },
 
     source_mets_file => qr/^mdl\.\w+\.\w{3}\d{5}\w?-all\.xml$/,
@@ -55,25 +56,26 @@ our $config = {
     # Don't validate consistency for MDL Contone composite images -- there will not
     # always be an OCR image for every page image and seq numbers need not be sequential.
     validation_run_stages => [
-        qw(validate_file_names
-          validate_filegroups_nonempty
-          validate_checksums
-          validate_utf8
-          validate_metadata)
+    qw(validate_file_names
+    validate_filegroups_nonempty
+    validate_checksums
+    validate_utf8
+    validate_metadata)
     ],
 
-    # The list of stages to run to successfully ingest a volume.
-    stages_to_run => [qw(
-        HTFeed::VolumeValidator
-        HTFeed::Stage::Pack
-        HTFeed::PackageType::MDLContoneComposite::METS
-        HTFeed::Stage::Handle
-        HTFeed::Stage::Collate
-	)],
+    # what stage to run given the current state
+    stage_map => {
+        ready      => 'HTFeed::PackageType::MDLContone::Unpack',
+        unpacked   => 'HTFeed::VolumeValidator',
+        validated  => 'HTFeed::Stage::Pack',
+        packed     => 'HTFeed::PackageType::MDLContoneComposite::METS',
+        metsed     => 'HTFeed::Stage::Handle',
+        handled    => 'HTFeed::Stage::Collate',
+    },
 
     module_validators => {
         'jp2'  => 'HTFeed::ModuleValidator::JPEG2000_hul',
-	'tif' => 'HTFeed::ModuleValidator::TIFF_hul'
+        'tif' => 'HTFeed::ModuleValidator::TIFF_hul'
     },
 
     uncompressed_extensions => ['jp2','tif'],
