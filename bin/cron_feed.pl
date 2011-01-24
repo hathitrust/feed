197deb3ca8ec443f5d2c694e7819aee66da1eb42
Aaron Elkiss <aelkiss@umich.edu>
2011-01-24 13:32:02 -0500

    Add special hacky logging to support generation of ingest log

--- bin/cron_feed.pl
+++ bin/cron_feed.pl
@@ -185,6 +185,7 @@ sub run_stage {
         $sth = HTFeed::DBTools::get_dbh()->prepare(
             q(UPDATE `queue` SET `status` = ?, failure_count=failure_count+1 WHERE `ns` = ? AND `pkg_type` = ? AND `objid` = ?;)
         );
+        HTFeed::DBTools::ingest_log_failure($volume,$stage,$status);
     }
     $sth->execute( $status, $namespace, $packagetype, $objid );
     
