805d1f8013c09597679326725777c58133296243
Aaron Elkiss <aelkiss@umich.edu>
2011-02-28 14:28:59 -0500

    Fix bit rot in ingest_test; allow restarting from any ready point

--- bin/ingest_test.pl
+++ bin/ingest_test.pl
@@ -4,8 +4,9 @@ use warnings;
 use strict;
 
 use HTFeed::Volume;
-use HTFeed::Log {root_logger => 'INFO, screen'};
+use HTFeed::Log {root_logger => 'TRACE, screen'};
 use Getopt::Long;
+use HTFeed::StagingSetup;
 
 # autoflush STDOUT
 $| = 1;
@@ -18,18 +19,24 @@ GetOptions ( "i" => \$ignore_errors, "n" => \$no_delete);
 my $packagetype = shift;
 my $namespace = shift;
 my $objid = shift;
+my $startstate  = shift;
 
 unless ($objid and $namespace and $packagetype){
-    print "usage: [-i] [-n] ingest_test.pl packagetype namespace objid\n";
+    print "usage: [-i] [-n] ingest_test.pl packagetype namespace objid [ state ]\n";
     exit 0;
 }
 
-print sprintf("Test ingest of %s %s %s commencing...\n",$packagetype,$namespace,$objid);
+HTFeed::StagingSetup::make_stage();
 
 my $stage;
 my $errors = 0;
 my $volume = HTFeed::Volume->new(objid => $objid,namespace => $namespace,packagetype => $packagetype);
-foreach my $stage_name ( @{$volume->get_stages()} ){
+
+my $stagelist = $volume->get_stages($startstate);
+print sprintf("Test ingest of %s %s %s commencing...\n",$packagetype,$namespace,$objid);
+print "Running stages: \n\n" . join("\n",@$stagelist), "\n";
+print "-----\n";
+foreach my $stage_name (@$stagelist) {
     my $stage_volume = HTFeed::Volume->new(objid => $objid,namespace => $namespace,packagetype => $packagetype);
     my $stage = eval "$stage_name->new(volume => \$stage_volume)";
     $errors += run_stage($stage);
@@ -42,7 +49,7 @@ foreach my $stage_name ( @{$volume->get_stages()} ){
 sub run_stage{
     my $stage = shift;
     
-    print "Running stage $stagename..\n";
+    print "Running stage " . ref($stage) . "..\n";
     
     $stage->run();
     if ($stage->succeeded()){
@@ -57,3 +64,4 @@ sub run_stage{
 
 print 'Volume ' . $volume->get_identifier() . " ingested unsuccessfully with $errors errors!\n" if ($errors);
 print 'Volume ' . $volume->get_identifier() . " ingested successfully!\n" unless ($errors);
+
