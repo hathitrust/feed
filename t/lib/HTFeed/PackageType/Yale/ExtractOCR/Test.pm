29c70ffbb0bff5dc1c1dfebb4e063e5aada26bf0
Ezra Brooks <ezbrooks@umich.edu>
2011-04-01 15:56:42 -0400

    Initial test::class structure for Yale unit tests

--- t/lib/HTFeed/PackageType/Yale/ExtractOCR/Test.pm
+++ t/lib/HTFeed/PackageType/Yale/ExtractOCR/Test.pm
@@ -2,14 +2,25 @@ package HTFeed::PackageType::Yale::ExtractOCR::Test;
 
 use warnings;
 use strict;
-
-use base qw(HTFeed::Stage::AbstractTest);
+use base qw(HTFeed::PackageType::Yale::AbstractTest);
+use HTFeed::Test::Support qw(get_fake_stage);
+use File::Path qw(make_path);
 use Test::More;
 
-#sub my_test : Test(1){
-#    my $self = shift;
-#    
-#}
+sub prep : Test(setup){
+	#setup method
+}
+
+sub ExtractOCR : Test(1){
+    my $self = shift;
+	my $stage = $self->{test_stage};
+	ok($stage->run(), 'Extract succeeded');
+}
+
+sub Failure : Test(){
+	#get damaged package
+	#test error conditions
+}
 
 1;
 
