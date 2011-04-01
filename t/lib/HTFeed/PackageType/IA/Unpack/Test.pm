f137cc1d6940d90924ed032f73cfa924c556910b
Ezra Brooks <ezbrooks@umich.edu>
2011-04-01 11:41:23 -0400

    Update unit tests for IA Stage testing

--- t/lib/HTFeed/PackageType/IA/Unpack/Test.pm
+++ t/lib/HTFeed/PackageType/IA/Unpack/Test.pm
@@ -4,12 +4,24 @@ use warnings;
 use strict;
 
 use base qw(HTFeed::Stage::AbstractTest);
+use HTFeed::Test::Support qw(get_fake_stage);
+use File::Path qw(make_path);
 use Test::More;
 
-#sub my_test : Test(1){
-#    my $self = shift;
-#    
-#}
+sub fixDir : Test(setup) {
+	#set config
+}
+
+sub Unpack : Test(1){
+    my $self = shift;
+	my $volume = $self->{volume};
+	my $stage = $self->{test_stage};
+	ok($stage->run, 'Unpacked');
+}
+
+sub Errors : Test{
+	# TODO test ram_disk_size error
+}
 
 1;
 
