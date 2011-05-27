05f4d57d3a1cc9b2696610faad8730454c5c6acf
Ryan Rotter <rrotter@umich.edu>
2011-05-27 12:29:54 -0400

    Automatically test_config('original') after each test

--- t/lib/HTFeed/Test/Class.pm
+++ t/lib/HTFeed/Test/Class.pm
@@ -1,6 +1,7 @@
 package HTFeed::Test::Class;
 
 use base qw(Test::Class);
+use HTFeed::Test::Support qw(test_config);
 
 # return testing class, with assumption that $class eq "$testing_class::Test"
 sub testing_class{
@@ -8,6 +9,12 @@ sub testing_class{
     my $class = ref $self;
     $class =~ s/::Test$//;
     return $class;
+}
+
+# return Config.pm settings to their initial state in case they have been changes
+sub global_test_teardown : Test(teardown){
+    HTFeed::StagingSetup::clear_stage;
+    test_config('original');
 }
 
 1;
