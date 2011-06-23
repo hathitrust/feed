0584fe0fea21848db6914e7a340c074ac15bf1e3
Aaron Elkiss <aelkiss@umich.edu>
2011-06-23 15:41:49 -0400

    Add additional comments for XMP header stuff

--- bin/compress_tif_jp2.pl
+++ bin/compress_tif_jp2.pl
@@ -8,6 +8,8 @@ use HTFeed::Config qw(get_config);
 use List::Util qw(max);
 use POSIX qw(ceil);
 
+# This script shows how to compress TIFF to JPEG2000 images and add an XMP
+# using kakadu and exiftool. Feel free to modify to your own needs.
 
 
 my $self = new HTFeed::Stage::ImageRemediate(volume => new HTFeed::TestVolume(namespace => 'namespace', packagetype => 'pkgtype')); 
@@ -48,7 +50,8 @@ foreach my $infile (@ARGV) {
 
         and die("kdu_compress returned $?");
 
-    # then set new metadata fields
+    # then set new metadata fields: copy from exiftool field called
+    # IFD0:whatever to XMP-tiff:whatever, where the fields have the same name
     foreach $field  ( qw(ImageWidth ImageHeight BitsPerSample 
         PhotometricInterpretation Orientation 
         SamplesPerPixel XResolution YResolution 
@@ -56,12 +59,19 @@ foreach my $infile (@ARGV) {
         $self->copy_old_to_new("IFD0:$field","XMP-tiff:$field");
     }
 
+    # Copy IFD0:Modifydate to XMP-tiff:DateTime
     $self->copy_old_to_new("IFD0:ModifyDate","XMP-tiff:DateTime");
     my $docname = $self->{oldFields}->{'IFD0:DocumentName'};
+
+    # Migrate the DocumentName field to the XMP dc:source field, updating
+    # the extension
     $docname =~ s/\.tif$/\.jp2/;
     $self->set_new_if_undefined("XMP-dc:source",$docname);
+
+    # Set the XMP-tiff:Compression header to JPEG 2000
     $self->set_new_if_undefined("XMP-tiff:Compression","JPEG 2000");
 
+    # Actually do the work of setting the fields with ExifTool
     my $exifTool = new Image::ExifTool;
     while ( ( $field, $val ) = each(%{$self->{newFields}}) ) {
         my ( $success, $errStr ) = $exifTool->SetNewValue( $field, $val );
