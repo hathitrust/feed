2ddae6c619f2d723f0b3db5bfea8cda2377dd0a8
Ryan Rotter <rrotter@umich.edu>
2011-07-25 17:46:34 -0400

    prepare_set 	limit by source, not ns

--- bin/dataset/prepare_set.pl
+++ bin/dataset/prepare_set.pl
@@ -10,22 +10,32 @@ use HTFeed::Volume;
 use HTFeed::StagingSetup;
 use HTFeed::Version;
 
-HTFeed::StagingSetup::make_stage(1);
+use Getopt::Long;
+
+my $pid = $$;
 
 my $get_partitions = 0;
 my ($start_date,$end_date);
 
-my @ns = ('uc2','nnc2');
-#my @ns = ('mdp');
+GetOptions ("generate=i" => \$get_partitions,
+            "start=s"    => \$start_date,
+            "end=s"      => \$end_date          ) or die;
+
 
 # generate base query for finding pd volumes in rights_current
 my $rights_current_base_q = 'mdp.rights_current WHERE ';
-$rights_current_base_q   .= '(namespace = \'' . (join '\' OR namespace = \'', @ns) . '\') AND '
-    if (@ns);
+# limit by namespace
+#$rights_current_base_q   .= '(namespace = \'' . (join '\' OR namespace = \'', @ns) . '\') AND '
+#    if (@ns);
+# limit by soucre (non-google)
+$rights_current_base_q   .= 'source != 1 AND ';
+# limit by attr (pd, pdus, cc, etc)
 $rights_current_base_q   .= '(attr = 1 OR attr = 7 OR (attr > 8 AND attr < 16))';
+
 
 #### 
 # Handle -g (generate partitioned commands) flag
+#
 ####
 if ($get_partitions){
 
@@ -86,8 +96,10 @@ if ($get_partitions){
 # default operation - process volume a dditions for specified date range
 ####
 
+HTFeed::StagingSetup::make_stage(1);
+
 my $kids = 0;
-my $max_kids = 16;
+my $max_kids = 0;
 
 # get namespace,id for selected date range
 my $rights_current_date_range_q = "SELECT namespace,id FROM $rights_current_base_q";
@@ -141,8 +153,14 @@ sub spawn_volume_adder{
     }
     elsif (defined $pid){
         HTFeed::Dataset::add_volume($volume);
+        exit(0);
     }
     else {
         die "Couldn't fork: $!";
     }
+}
+
+END{
+    HTFeed::StagingSetup::clear_stage()
+        if ($$ eq $pid);
 }
