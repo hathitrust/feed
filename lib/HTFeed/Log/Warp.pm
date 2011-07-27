2d5a5a0260dc6b945b687492cdddefc5b899c1ff
Aaron Elkiss <aelkiss@umich.edu>
2011-07-27 15:02:48 -0400

    Log running script in stage if stage is not set

--- lib/HTFeed/Log/Warp.pm
+++ lib/HTFeed/Log/Warp.pm
@@ -9,6 +9,11 @@ sub toDBArray {
     my $error_message = HTFeed::Log::error_code_to_string(shift);
     my %message_fields = @_;
 
+    # log running script if stage is not defined
+    if(not defined $message_fields{stage}) {
+        $message_fields{stage} = $0;
+    }
+
     my $error_array = HTFeed::Log::fields_hash_to_array(\%message_fields);
 
     return ($error_message, @$error_array);
