5e2efdec0b8be09adc70b82668ca48eff301cfba
Ryan Rotter <rrotter@umich.edu>
2011-09-29 12:45:00 -0400

    Updated queue/priority API added priority options to Queue methods exposed priority options in enqueue.pl fixed Priority::set_item_priority, it didn't work due to faulty SQL query added Priority::rebin()

--- lib/HTFeed/DBTools/Priority.pm
+++ lib/HTFeed/DBTools/Priority.pm
@@ -41,7 +41,7 @@ use Carp;
 use Switch;
 use base qw(HTFeed::DBTools);
 
-our @EXPORT_OK = qw(reprioritize initial_priority set_item_priority group_priority);
+our @EXPORT_OK = qw(reprioritize initial_priority set_item_priority rebin);
 
 =item set_item_priority
 set the priority for a volume
@@ -56,22 +56,22 @@ sub set_item_priority{
     my $ns = $volume->get_namespace();
     my $pkg_type = $volume->get_packagetype();
     my $id = $volume->get_objid();
-    
-    my $modifier = shift;
-     
+
+    my $priority_modifier = shift;
+
     my $priority = 0;
-    switch ($modifier){
+    switch ($priority_modifier){
         case "first"        { $priority = $FIRST }
         case "last"         { $priority = $LAST }
         case "group_first"  { $priority = group_priority($ns,$pkg_type) * $GROUP_OFF + 0x1 }
         case "group_last"   { $priority = group_priority($ns,$pkg_type) * $GROUP_OFF + 0x3 }
         else                { $priority = group_priority($ns,$pkg_type) * $GROUP_OFF + 0x2 }
     }
-    
+
     my $sth = HTFeed::DBTools::get_dbh()->prepare(qq(UPDATE queue SET priority = (priority & $BIN_MASK) + ? WHERE namespace = ? AND id = ?;));
-    
-    $sth->execute($priority,$ns,$pkg_type);
-    
+
+    $sth->execute($priority,$ns,$id);
+
     return;
 }
 
@@ -87,10 +87,10 @@ sub group_priority{
     # bind each var twice to accommodate wacky SQL
     $get_group_priority->execute($ns,$pkg_type,$ns,$pkg_type);
     my ($group_priority) = $get_group_priority->fetchrow_array();
-    
+
     # if we didn't find a priority, default to last group
     $group_priority = $MAX if (! defined $group_priority);
-    
+
     return $group_priority;
 }
 
@@ -117,37 +117,68 @@ sub reprioritize{
     my $get_nspkgs = HTFeed::DBTools::get_dbh()->prepare(qq(SELECT DISTINCT namespace, pkg_type FROM queue WHERE $where_syntax;));
     my $mask = $BIN_MASK + $SUBG_MASK;
     my $update_priority = HTFeed::DBTools::get_dbh()->prepare(qq(UPDATE queue SET priority = (? * $GROUP_OFF) + (priority & $mask) WHERE namespace = ? AND pkg_type = ? AND $where_syntax;));
-    
+
     $get_nspkgs->execute();
     while(my ($ns,$pkg) = $get_nspkgs->fetchrow_array()){
         my $priority = group_priority($ns,$pkg);
         $update_priority->execute($priority,$ns,$pkg);
-        
+
         #print "$ns,$pkg,$priority\n";
     }
-    
+
     return;
 }
 
 =item initial_priority
 returns the initial priority the Queue.pm should set on an item before running reprioritize()
-
-this is currently the only place where BIN is set
+=synopsis
+initial_priority();
+initial_priority($modifier);
 =cut
 sub initial_priority{
     my $volume = shift;
-    my $reingest = $volume->ingested();
+    my $priority_modifier = shift;
 
     # default bin is 1
     my $bin = 1;
-    
-    # increment bin for anything that makes the volume a lower priority
-    if ($reingest){
-        $bin++;
+
+    my $priority;
+
+    switch ($priority_modifier){
+        # first and last have no group, so skip the flag
+        case "first"        { $priority = $FIRST }
+        case "last"         { $priority = $LAST }
+        # set priority with 'new' flag (from $NEW_MASK) to set group later
+        case "group_first"  { $priority = $NEW_MASK + $bin * $BIN_OFF + 0x1 }
+        case "group_last"   { $priority = $NEW_MASK + $bin * $BIN_OFF + 0x3 }
+        else                { $priority = $NEW_MASK + $bin * $BIN_OFF + 0x2 }
     }
-    my $priority = $NEW_MASK + $bin * $BIN_OFF + 0x2;
 
     return $priority;
+}
+
+=item rebin
+the use of the bin field has still not been fully fleshed out
+for now, this will put item into bin 2 by default
+
+any future default behavior will always to be to demote (i.e. increase) the bin an some way
+=synopsis
+rebin($volume); # demote item
+rebin($volume[,$bin]); # place item in bin $bin (1-0xFF)
+=cut
+sub rebin{
+    my $volume = shift;
+    my $bin = (shift or 2);
+    die "$bin is an invalid bin, bin must be between 1 and $MAX"
+        unless (defined $bin and $bin > 0 and $bin <= $MAX);
+
+    my $ns = $volume->get_namespace();
+    my $id = $volume->get_objid();
+
+    # clear bin, set bin, don't touch it if priority is FIRST or LAST
+    my $sth = HTFeed::DBTools::get_dbh()->prepare(qq(UPDATE queue SET priority = (priority & ~$BIN_MASK) + ? WHERE namespace = ? AND id = ? AND priority != $FIRST AND priority < $LAST;));
+
+    $sth->execute($bin*$BIN_OFF,$ns,$id);
 }
 
 1;
