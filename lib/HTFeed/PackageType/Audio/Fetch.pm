efaf105fbb4b0b9b4ae7d50ac2d5b5aea90011a7
Ezra Brooks <ezbrooks@umich.edu>
2011-11-03 13:27:50 -0400

    Updates to Audio Fetch process

---
+++ lib/HTFeed/PackageType/Audio/Fetch.pm
@@ -0,0 +1,26 @@
+package HTFeed::PackageType::Audio::Fetch;
+
+use strict;
+use warnings;
+use base qw(HTFeed::Stage::Fetch);
+use Log::Log4perl qw(get_logger);
+use HTFeed::Config qw(get_config);
+
+sub run {
+
+	my $self = shift;
+	my $volume = $self->{volume};
+	my $packagetype = $volume->get_packagetype();
+	my $objid = $volume->get_objid;
+
+	my $source = get_config('staging' => 'fetch');
+	my $dest = get_config('staging' => 'preingest');
+
+	$self->fetch_from_source($source,$dest);
+
+	$self->_set_done();
+	return $self->succeeded();
+
+}
+
+1;
