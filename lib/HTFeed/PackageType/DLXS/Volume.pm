2bcd49f2850262dd1f4a43de8f3e8a3f365d9126
Aaron Elkiss <aelkiss@umich.edu>
2012-03-15 15:15:58 -0400

    Flip around inheritance relationship between MPubDCU::Volume and DLXS::Volume for clarity

--- lib/HTFeed/PackageType/DLXS/Volume.pm
+++ lib/HTFeed/PackageType/DLXS/Volume.pm
@@ -2,18 +2,9 @@ package HTFeed::PackageType::DLXS::Volume;
 
 use warnings;
 use strict;
-use base qw(HTFeed::PackageType::MPubDCU::Volume);
+use base qw(HTFeed::Volume);
+use HTFeed::PackageType::MPubDCU::Volume;
 use HTFeed::Config;
-
-# no source METS expected for this content 
-
-sub get_source_mets_xpc {
-    return;
-}
-
-sub get_source_mets_file {
-    return;
-}
 
 # don't pt-escape the directory name for preingest for these (following dlxs conventions)
 sub get_preingest_directory {
@@ -24,6 +15,48 @@ sub get_preingest_directory {
     return sprintf("%s/%s", get_config('staging'=>'disk'=>'preingest'), $objid) if $ondisk;
     return sprintf("%s/%s", get_config('staging'=>'preingest'), $objid);
 }
+
+sub get_page_data {
+    my $self = shift;
+    my $file = shift;
+
+    (my $seqnum) = ($file =~ /(\d+)\./);
+    croak("Can't extract sequence number from file $file") unless $seqnum;
+
+    if(not defined $self->{'page_data'}) {
+        my $pagedata = {};
+
+        my $pageview = $self->get_staging_directory() . "/pageview.dat";
+        if(-e $pageview) {
+            open(my $pageview_fh,"<$pageview") or croak("Can't open pageview.dat: $!");
+            <$pageview_fh>; # skip first line - column headers
+            while(my $line = <$pageview_fh>) {
+                # clear line endings
+                $line =~ s/[\r\n]//;
+                my(undef,$order,$detected_pagenum,undef,$tags) = split(/\t/,$line);
+                $detected_pagenum =~ s/^0+//; # remove leading zeroes from pagenum
+                if (defined $tags) {
+                    $tags = join(', ',split(/\s/,$tags));
+                }
+
+                $pagedata->{$order} = {
+                    orderlabel => $detected_pagenum,
+                    label => $tags
+                }
+            }
+            $self->{page_data} = $pagedata;
+        }
+    }
+
+    return $self->{page_data}{$seqnum};
+}
+
+# no download location to clean for this material
+
+sub get_download_location {
+    return;
+}
+
 
 1;
 
--- lib/HTFeed/PackageType/MPubDCU/Volume.pm
+++ lib/HTFeed/PackageType/MPubDCU/Volume.pm
@@ -2,7 +2,7 @@ package HTFeed::PackageType::MPubDCU::Volume;
 
 use warnings;
 use strict;
-use base qw(HTFeed::Volume);
+use base qw(HTFeed::PackageType::DLXS::Volume);
 use HTFeed::Config;
 
 # no source METS expected for this content 
@@ -15,6 +15,7 @@ sub get_source_mets_file {
     return;
 }
 
+# use checksum.md5 instead of source METS
 sub get_checksums{
 
 	my $self = shift;
@@ -42,46 +43,11 @@ sub get_checksums{
     return $self->{checksums};
 }
 
-sub get_page_data {
-    my $self = shift;
-    my $file = shift;
-
-    (my $seqnum) = ($file =~ /(\d+)\./);
-    croak("Can't extract sequence number from file $file") unless $seqnum;
-
-    if(not defined $self->{'page_data'}) {
-        my $pagedata = {};
-
-        my $pageview = $self->get_staging_directory() . "/pageview.dat";
-        if(-e $pageview) {
-            open(my $pageview_fh,"<$pageview") or croak("Can't open pageview.dat: $!");
-            <$pageview_fh>; # skip first line - column headers
-            while(my $line = <$pageview_fh>) {
-                # clear line endings
-                $line =~ s/[\r\n]//;
-                my(undef,$order,$detected_pagenum,undef,$tags) = split(/\t/,$line);
-                $detected_pagenum =~ s/^0+//; # remove leading zeroes from pagenum
-                if (defined $tags) {
-                    $tags = join(', ',split(/\s/,$tags));
-                }
-
-                $pagedata->{$order} = {
-                    orderlabel => $detected_pagenum,
-                    label => $tags
-                }
-            }
-            $self->{page_data} = $pagedata;
-        }
-    }
-
-    return $self->{page_data}{$seqnum};
-}
-
-# no download location to clean for this material
-
-sub get_download_location {
+# no preingest/remediation for this content
+sub get_preingest_directory {
     return;
 }
+
 
 1;
 
