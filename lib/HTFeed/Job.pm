57bfa7a9ab267bf42d2d41bfd8d6d4fe93dadd83
Ryan Rotter <rrotter@umich.edu>
2012-04-09 15:52:49 -0400

    Changes to RunLite and Job to get RunLite done and working. Use ServerStatus::check_disk_usage in feedd

--- lib/HTFeed/Job.pm
+++ lib/HTFeed/Job.pm
@@ -12,9 +12,10 @@ has [qw(pkg_type namespace id)]         => (is => 'ro', isa => 'Str',     lazy_b
 has 'status'                            => (is => 'ro', isa => 'Str',     required => 1, default => 'ready');
 has 'callback'                          => (is => 'ro', isa => 'CodeRef', required => 1);
 has 'failure_count'                     => (is => 'rw', isa => 'Str',     required => 1, default => 0);
-has 'stage_class'                       => (is => 'ro', isa => 'Maybe[Str]',     init_arg => undef, lazy_build => 1);
+has 'stage_class'                       => (is => 'ro', isa => 'Maybe[Str]', init_arg => undef, lazy_build => 1);
 has 'new_status'                        => (is => 'ro', isa => 'Str',     init_arg => undef, lazy_build => 1);
-has 'volume'                            => (is => 'ro', isa => 'Object',  init_arg => undef, lazy_build => 1);
+has '_release'                          => (is => 'ro', isa => 'Bool',    init_arg => undef, lazy_build => 1);
+has 'volume'                            => (is => 'ro', isa => 'Object',                     lazy_build => 1);
 has 'stage'                             => (is => 'ro', isa => 'Object',  init_arg => undef, lazy_build => 1);
 
 =item new
@@ -59,17 +60,31 @@ sub update{
     ## i.e. put it somwhere else, but preferably somthing tidy
     my %release_states = map {$_ => 1} @{get_config('daemon'=>'release_states')};
 
-    my $release = 0;
-    $release = 1 if (defined $release_states{$new_status});
+    my $release = $self->_release;
 
     get_logger()->info( 'StageSucceeded', objid => $self->id, namespace => $self->namespace, stage => $self->stage_class, detail => $stage->success_info() )
         if (!$fail);
     get_logger()->info( 'StageFailed', objid => $self->id, namespace => $self->namespace, stage => $self->stage_class, detail => 'fatal=' . ($new_status eq 'punted' ? '1' : '0') )
         if ($fail);
-    
+
     &{$self->{callback}}($self->namespace, $self->id, $new_status, $release, $fail);
 
     return;
+}
+
+sub _build__release{
+    my $self = shift;
+
+    my $new_status = $self->new_status;
+
+    ## TODO: make this a class global or see if it can be better accessed with YAML::Config, etc.
+    ## i.e. put it somwhere else, but preferably somthing tidy
+    my %release_states = map {$_ => 1} @{get_config('daemon'=>'release_states')};
+
+    my $release = 0;
+    $release = 1 if (defined $release_states{$new_status});
+
+    return $release;
 }
 
 =item clean
@@ -253,13 +268,17 @@ returns false if we have reached a release state
 sub successor {
     my $self = shift;
 
+	return if($self->_release());
+
     my $failure_count = $self->failure_count + $self->stage->failed;
     my $status = $self->new_status;
 
-    my $successor = __CLASS__->new(volume => $self->volume,
+    my $successor = HTFeed::Job->new(volume => $self->volume,
                     status => $self->new_status,
                     failure_count => ($failure_count),
-                    callback => \&callback)
+                    callback => $self->callback);
+	
+	return $successor;
 }
 
 1;
