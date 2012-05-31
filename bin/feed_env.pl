fb2a4494884f15883952b84ddde95817aa906464
Ryan Rotter <rrotter@umich.edu>
2012-05-31 15:12:30 -0400

    Added install and configuration docs Fixed typo in config file comments

--- bin/feed_env.pl
+++ bin/feed_env.pl
@@ -8,6 +8,12 @@ use lib "$FindBin::Bin/../lib";
 use HTFeed;
 use HTFeed::Config qw(get_config);
 
-HTFeed::Config::_dump();
+unless (scalar @ARGV) {
+    HTFeed::Config::_dump();
+    exit 0;
+}
+
+my $value = get_config($ARGV[0]);
+print "$value\n";
 
 1;
