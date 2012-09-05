4cd2583caa9342b3ee2e5e04b5845b32d0470e14
Aaron Elkiss <aelkiss@umich.edu>
2012-09-05 16:29:33 -0400

    pass parameters from start_audit to fscrawl

--- start_audit.sh
+++ start_audit.sh
@@ -5,5 +5,5 @@ export HTFEED_CONFIG=$HTFEEDROOT/etc/config.yaml
 export PERL5LIB=$HTFEEDROOT/lib
 export LD_LIBRARY_PATH=$HTFEEDROOT/xerces-c-3.1.0/lib:$LD_LIBRARY_PATH
 for dir in `seq $1 2 24`; 
-    do perl -w $HTFEEDROOT/bin/audit/fsCrawl.pl "/sdr$dir/obj" 2>&1 > $HTFEEDROOT/var/audit/sdr$dir-$TODAY.log &
+    do perl -w $HTFEEDROOT/bin/audit/fsCrawl.pl $@ "/sdr$dir/obj" 2>&1 > $HTFEEDROOT/var/audit/sdr$dir-$TODAY.log &
 done
