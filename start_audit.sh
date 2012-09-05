2398b425b6e47429fc373c27933549367572a8ab
Aaron Elkiss <aelkiss@umich.edu>
2012-09-05 16:34:55 -0400

    fix parameter passing; don't force outdated config for start_audit

--- start_audit.sh
+++ start_audit.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 HTFEEDROOT=/htapps/babel/feed
 TODAY=`date +"%Y%m%d"`
-export HTFEED_CONFIG=$HTFEEDROOT/etc/config.yaml
 export PERL5LIB=$HTFEEDROOT/lib
 export LD_LIBRARY_PATH=$HTFEEDROOT/xerces-c-3.1.0/lib:$LD_LIBRARY_PATH
-for dir in `seq $1 2 24`; 
+ORIGDIR=$1
+shift
+for dir in `seq $ORIGDIR 2 24`; 
     do perl -w $HTFEEDROOT/bin/audit/fsCrawl.pl $@ "/sdr$dir/obj" 2>&1 > $HTFEEDROOT/var/audit/sdr$dir-$TODAY.log &
 done
