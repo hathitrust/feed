3895a76018bf878fcb50ae91d4fe9a039e4d3c63
Aaron Elkiss <aelkiss@umich.edu>
2013-04-25 16:20:13 -0400

    handle aberrant agent IDs in old METS

--- lib/HTFeed/PackageType/MPub/METS.pm
+++ lib/HTFeed/PackageType/MPub/METS.pm
@@ -142,7 +142,11 @@ sub _extract_old_premis {
                     $eventconfig->{'executor_type'} = 'MARC21 Code';
                     $eventconfig->{'executor'} = 'Ca-MvGOO';
                     $from_tz = 'America/Los_Angeles';
-                } elsif($agentid eq 'UM' or $agentid eq 'SPO' or $agentid =~ /Digital.Conversion/) {
+                } elsif($agentid eq 'UM' 
+                        or $agentid eq 'SPO' 
+                        or $agentid =~ /Digital.Conversion/
+                        or $agentid =~ /MPublishing/
+                ) {
                     $eventconfig->{'executor_type'} = 'MARC21 Code';
                     $eventconfig->{'executor'} = 'MiU';
                     $from_tz = 'America/Detroit';
