9369cc248e44df39efa4b7738ce4d8f8ccf8b5a4
Aaron Elkiss <aelkiss@umich.edu>
2013-05-16 10:34:50 -0400

    add email load script

---
+++ bin/load_email.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use HTFeed::Log { root_loger => 'INFO, scree' };
+use HTFeed::RunLite qw(runlite);
+use METS;
+use HTFeed::METS;
+
+# process a single Bentley barcode
+
+my $id = shift;
+unless($id) {
+	print "Please specify a barcode\n";
+	exit;
+}
+
+my $volumes = [['bhl', $id]];
+
+runlite(
+	volumes		=> $volumes,
+	namespace	=> 'bhl',
+	packagetype	=> 'email',
+	verbose		=> 1
+);
+
+__END__
+
