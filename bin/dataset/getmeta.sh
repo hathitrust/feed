c0ad1926ad9dce49548e02f15abb3cf519f83423
Aaron Elkiss <aelkiss@umich.edu>
2013-10-09 14:24:05 -0400

    New format, used first on bib-only ht_all; refresh special case symlink for ht_all bib data.

--- bin/dataset/getmeta.sh
+++ bin/dataset/getmeta.sh
@@ -4,6 +4,10 @@
 /usr/bin/scp aleph@irene:/exlibris/aleph/mdp_meta_transfer/\*.tar.gz /htprep/datasets/meta/
 /usr/bin/ssh aleph@irene /bin/rm -f /exlibris/aleph/mdp_meta_transfer/\*.tar.gz
 
+# New format, used first on bib-only ht_all.
+/usr/bin/scp aleph@irene:/exlibris/aleph/mdp_meta_transfer/\*.json.gz /htprep/datasets/meta/
+/usr/bin/ssh aleph@irene /bin/rm -f /exlibris/aleph/mdp_meta_transfer/\*.json.gz
+
 # Delete any old bundles.
 cd /htprep/datasets/meta
 for prefix in `/bin/ls -1 *-* | /bin/cut -f 1 -d "-"| /bin/sort | /usr/bin/uniq`; do
@@ -15,3 +19,7 @@ for prefix in `/bin/ls -1 *-* | /bin/cut -f 1 -d "-"| /bin/sort | /usr/bin/uniq`
     done
   fi
 done
+
+# Refresh special case symlink for ht_all bib data.
+rm -f /htprep/datasets/ht_all/obj/meta.json.gz
+ln -s /htprep/datasets/meta/ht_all*.gz /htprep/datasets/ht_all/obj/meta.json.gz
