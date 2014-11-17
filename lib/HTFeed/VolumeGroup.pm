c3ea3d42649e5ee291a008b3a1b91d1b4d5e7eaf
Ryan Rotter <rrotter@umich.edu>
2014-11-17 11:29:49 -0500

    Print better errors in VolumeGroup.pm

--- lib/HTFeed/VolumeGroup.pm
+++ lib/HTFeed/VolumeGroup.pm
@@ -143,7 +143,8 @@ sub shift {
             if ($ns_objid);
     };
     if ($@) {
-        warn "Error $@ instantiating volume $ns_objid";
+        $htid ||= join '.', @{$nsobjid};
+        warn "Error $@ instantiating volume $htid";
         return $self->shift;
     }
 
