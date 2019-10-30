a6ca2e6f256290e47840e6e860f35f96ff1fda23
Aaron Elkiss <aelkiss@umich.edu>
2019-10-30 11:59:28 -0400

    HT-1810: Add sample configuration

---
+++ etc/sample_namespace/TEST.pm
@@ -0,0 +1,25 @@
+package HTFeed::Namespace::TEST;
+
+# For testing
+
+use base qw(HTFeed::Namespace);
+
+# The HathiTrust namespace
+our $identifier = 'test';
+
+our $config = {
+    handle_prefix => '2027/deleteme',
+    packagetypes => [qw(ht ia simple kirtas epub simpledigital vendoraudio)],
+    description => 'Test namespace',
+
+    default_timezone => 'America/Detroit',
+};
+
+# Everything is permitted. Nothing is forbidden.
+sub validate_barcode {
+    return 1;
+}
+
+1;
+
+__END__
