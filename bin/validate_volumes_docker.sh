b0bb7c19d25d3352fd03ad9886c3c7f943feb333
Aaron Elkiss <aelkiss@umich.edu>
2019-10-30 16:01:16 -0400

    HT-1618: validate volume in a docker container
    - Uses the default jhove in stretch
    - Pulls in sample config & test namespace
    
    It's a start, but it doesn't work for volumes with JPEG2000 images,
    though, because kakadu is not yet installed.

---
+++ bin/validate_volumes_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+fullpath=$(realpath $1)
+dir=$(dirname $fullpath)
+objid=$(basename $fullpath .zip)
+
+docker run --mount type=bind,source=$dir,target=/tmp/stage/toingest/test,readonly hathitrust/feed:imagevalidate bash -c "HTFEED_CONFIG=/usr/local/etc/feed/config_prevalidate.yaml perl -w bin/validate_volume.pl -1 simple test $objid --no-clean"
