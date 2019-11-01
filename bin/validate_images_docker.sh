4fd13434d3bc052521d730ecefd446083511bcfe
Aaron Elkiss <aelkiss@umich.edu>
2019-11-01 12:53:10 -0400

    HT-1618: add scripts to run image & full directory validation

---
+++ bin/validate_images_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+fullpath=$(realpath $1)
+
+docker run --mount type=bind,source=$fullpath,target=/tmp/images_to_validate,readonly hathitrust/feed:imagevalidate bash -c "HTFEED_CONFIG=/usr/local/etc/feed/config_prevalidate.yaml perl -w /usr/local/feed/bin/validate_images.pl simple test /tmp/images_to_validate --no-clean"
---
+++ bin/validate_volume_directory_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+fullpath=$(realpath $1)
+
+docker run --mount type=bind,source=$fullpath,target=/tmp/stage/toingest/test,readonly hathitrust/feed:imagevalidate bash -c "/bin/ls /tmp/stage/toingest/test | sed s/.zip// | HTFEED_CONFIG=/usr/local/etc/feed/config_prevalidate.yaml perl -w /usr/local/feed/bin/validate_volume.pl -p simple -n test --no-clean"
