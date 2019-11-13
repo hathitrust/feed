fe7c219f4a5540ade3d97be4db54b24699ddbb08
Aaron Elkiss <aelkiss@umich.edu>
2019-11-13 13:28:33 -0500

    Audit: adjust paths to reflect new deployment location

--- start_audit.sh
+++ start_audit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-HTFEEDROOT=/htapps/babel/feed
+SDRROOT=/htapps/babel
+FEED_HOME=$SDRROOT/feed
 TODAY=`date +"%Y%m%d"`
-export PERL5LIB=$HTFEEDROOT/lib
-export LD_LIBRARY_PATH=$HTFEEDROOT/xerces-c-3.1.0/lib:$LD_LIBRARY_PATH
+export PERL5LIB=$FEED_HOME/lib
+export LD_LIBRARY_PATH=$FEED_HOME/xerces-c-3.1.0/lib:$LD_LIBRARY_PATH
 ORIGDIR=$1
 shift
 for dir in `seq $ORIGDIR 2 24`; 
-    do perl -w $HTFEEDROOT/bin/audit/fsCrawl.pl $@ "/sdr$dir/obj" 2>&1 > $HTFEEDROOT/var/audit/sdr$dir-$TODAY.log &
+    do perl -w $SDRROOT/audit/fsCrawl.pl $@ "/sdr$dir/obj" 2>&1 > $SDRROOT/audit/log/sdr$dir-$TODAY.log &
 done
