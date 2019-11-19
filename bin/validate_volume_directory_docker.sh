cab0e0ef1e9d711d67f60eb7e9fd4e844ecd04fc
Aaron Elkiss <aelkiss@umich.edu>
2019-11-19 13:37:40 -0500

    Remove support for JHOVE 1.6; rename jhove 1.20 image

--- bin/validate_volume_directory_docker.sh
+++ bin/validate_volume_directory_docker.sh
@@ -2,4 +2,4 @@
 
 fullpath=$(realpath $1)
 
-docker run --mount type=bind,source=$fullpath,target=/tmp/stage/toingest/test,readonly hathitrust/feed:jhove-1.20 bash -c "/bin/ls /tmp/stage/toingest/test | sed s/.zip// | HTFEED_CONFIG=/usr/local/etc/feed/config_prevalidate.yaml perl -w /usr/local/feed/bin/validate_volume.pl -p simple -n test --no-clean"
+docker run --mount type=bind,source=$fullpath,target=/tmp/stage/toingest/test,readonly hathitrust/feed:latest bash -c "/bin/ls /tmp/stage/toingest/test | sed s/.zip// | HTFEED_CONFIG=/usr/local/etc/feed/config_prevalidate.yaml perl -w /usr/local/feed/bin/validate_volume.pl -p simple -n test --no-clean"
