62c53bd811a77e9c99c736f0abb90be94373f098
Aaron Elkiss <aelkiss@umich.edu>
2019-12-16 10:36:28 -0500

    Set up environment for audit

--- start_audit.sh
+++ start_audit.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 SDRROOT=/htapps/babel
 FEED_HOME=$SDRROOT/feed
+HTFEED_CONFIG=$FEED_HOME/etc/config_ingest.yaml
 TODAY=`date +"%Y%m%d"`
-export PERL5LIB=$FEED_HOME/lib
+export PERL5LIB=$FEED_HOME/lib:$FEED_HOME/metslib:$FEED_HOME/google/lib
+export HTFEED_CONFIG
+export FEED_HOME
 export LD_LIBRARY_PATH=$FEED_HOME/xerces-c-3.1.0/lib:$LD_LIBRARY_PATH
 ORIGDIR=$1
 shift
