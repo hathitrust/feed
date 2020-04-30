b3ee282f8dd5671ca948999cf9b293aa57ad7c19
Aaron Elkiss <aelkiss@umich.edu>
2020-04-30 10:23:41 -0400

    Add ability to test incoming log messages
    - Define "string" appender in config file
    
    - Make sure that if we have multiple appenders defined, the first one
    that runs doesn't mutate the message

---
+++ t/lib/HTFeed/Test/Logger.pm
@@ -0,0 +1,24 @@
+package HTFeed::Test::Logger;
+
+use HTFeed::Log {root_logger => 'TRACE, string'};
+use Log::Log4perl::Appender::String;
+use Log::Log4perl qw(get_logger);
+
+sub new {
+  my $class = shift;
+  my $self = { appender => Log::Log4perl->appender_by_name("string") };
+  return bless($self,$class);
+}
+
+sub matches {
+  my $self = shift;
+  my $pattern = shift;
+  return $self->{appender}->string() =~ $pattern;
+}
+
+sub reset {
+  my $self = shift;
+  $self->{appender}->string("");
+}
+
+1;
