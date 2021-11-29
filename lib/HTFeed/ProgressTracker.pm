bd749713a54e0b783d4c411a547190745194e257
Aaron Elkiss <aelkiss@umich.edu>
2021-11-29 11:09:26 -0500

    ProgressTracker WIP

--- lib/HTFeed/ProgressTracker.pm
+++ lib/HTFeed/ProgressTracker.pm
@@ -12,55 +12,75 @@ my $DEFAULT_REPORT_INTERVAL = 1000;
 
 sub new {
   my $class = shift;
-  my %params = @;
-
+  my %params = @_;
 
   my $self = bless {}, $class;
 
-  $self->{job} = $params->{job} || $ENV{'JOB_NAME'} || basename($0);
-  $self->{report_interval} = $params->{report_interval} || $DEFAULT_REPORT_INTERVAL;
-  my $success_interval = $params->{success_interval} || $ENV{'JOB_SUCCESS_INTERVAL'};
+  $self->{job} = $params{job} || $ENV{'JOB_NAME'} || basename($0);
+  $self->{report_interval} = $params{report_interval} || $DEFAULT_REPORT_INTERVAL;
+  my $success_interval = $params{success_interval} || $ENV{'JOB_SUCCESS_INTERVAL'};
 
-  $self->{job} = $job;
-  $self->{prom} = Net::Prometheus->new;
+  $self->{prom} = Net::Prometheus->new(
+    disable_process_collector => 1,
+    disable_perl_collector => 1
+  );
   $self->{ua} = LWP::UserAgent->new;
 
-  $self->{records_so_far} = {};
-  $self->{last_reported_records} = {};
+  $self->{labels} = {};
+  $self->{records_so_far} = 0;
+  $self->{last_reported_records} = 0;
   $self->{start_time} = time();
 
   if($success_interval) { 
     $self->success_interval->set($success_interval);
   }
 
-  $self->update_metrics;
-
   return $self;
 
 }
 
+sub start_stage {
+  my $self = shift;
+  my $stage = shift;
+
+  # final counts from previous stage
+  $self->update_metrics if $self->{stage};
+
+  $self->{labels} = { labels => [ 'stage' ] };
+  $self->{stage} = $stage;
+  $self->{start_time} = time();
+  $self->{records_so_far} = 0;
+
+  # new stage
+  $self->update_metrics;
+}
+
 sub inc {
   my $self = shift;
-  my $label = shift || "";
   my $amount = shift || 1;
 
-  $self->{records_so_far}{$label} += $amount;
+  $self->{records_so_far} += $amount;
 
-  if($self->{records_so_far}{$label} > 
-    $self->{last_reported_records}{$label} + $self->{report_interval}) {
+  if($self->{records_so_far} > 
+    $self->{last_reported_records} + $self->{report_interval}) {
     $self->update_metrics;
+  }
+}
+
+sub labels {
+  my $self = shift;
+  if($self->{stage}) {
+    { stage => $self->{stage} }
+  } else {
+    {}
   }
 }
 
 sub update_metrics {
   my $self = shift;
 
-  $self->duration->set(time() - $self->{start_time});
-
-  while(my ($label, $value) = each(%{$self->{records_so_far}})) { 
-    $self->records_processed->set({ stage => $label }, $value);
-    $self->{last_reported_records}{$label} = $value;
-  }
+  $self->duration->set($self->labels, time() - $self->{start_time});
+  $self->records_processed->set($self->labels, $self->{records_so_far});
 
   $self->push_metrics;
 
@@ -85,23 +105,24 @@ sub push_metrics {
 
 }
 
-sub duration {
-  my $self = shift;
-
-  $self->{duration} ||= $self->{prom}->new_gauge(
-    name => 'job_duration_seconds',
-    help => 'Time spend running job in seconds'
-  )
-
-}
-
 sub last_success {
   my $self = shift;
 
   $self->{last_success} ||= $self->{prom}->new_gauge(
     name => 'job_last_success',
-    help => 'Last Unix time when job successfully completed'
+    help => 'Last Unix time when job successfully completed',
   )
+}
+
+sub duration {
+  my $self = shift;
+
+  $self->{duration} ||= $self->{prom}->new_gauge(
+    name => 'job_duration_seconds',
+    help => 'Time spend running job in seconds',
+    %{$self->{labels}}
+  )
+
 }
 
 sub records_processed {
@@ -109,7 +130,8 @@ sub records_processed {
 
   $self->{records_processed} ||= $self->{prom}->new_gauge(
     name => 'job_records_processed',
-    help => 'Records processed by job'
+    help => 'Records processed by job',
+    %{$self->{labels}}
   )
 }
 
