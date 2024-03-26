8a4db1b4a3bb7bec730b55af5c37a847341bc0d2
mw <mwarin@umich.edu>
2024-03-26 13:50:21 -0400

    mostly operational?

--- lib/HTFeed/JobMetrics.pm
+++ lib/HTFeed/JobMetrics.pm
@@ -1,15 +1,64 @@
-package HTFeed::StageTimer;
+package HTFeed::JobMetrics;
 
 use strict;
 use warnings;
 
 use Prometheus::Tiny::Shared;
 
+# This module is responsible for measuring job performance in different
+# stages. It knows what kinds of things we want to measure, and passes
+# each measurement to a file by prometheus.
+
+### Usage:
+#
+### display the current metrics:
+# $ perl JobMetrics.pm --pretty
+#
+### just see the list of metrics
+# $ perl JobMetrics.pm --list
+#
+### irrevocably clear current metrics:
+# $ perl JobMetrics.pm --clear
+#
+### or:
+# use HTFeed::JobMetrics;
+# my $jm = HTFeed::JobMetrics->new();
+# $jm->inc("fetched_items");       # for counters (/*_items$/)
+# $jm->observe("fetched_ms", 220); # for historgrams (/*_(kb|ms)$/)
+# print $jm->pretty;
+### list available metrics
+# $jm->list_metrics
+
+if (!caller) { # If called from commandline:
+    my $jm = HTFeed::JobMetrics->new();
+
+    # pass --pretty to see all data pretty-printed
+    if (grep { /^--pretty$/ } @ARGV) {
+	print $jm->pretty . "\n";
+    }
+
+    # pass --list to see just the names of known metrics
+    if (grep { /^--list$/ } @ARGV) {
+	print join("\n", @{$jm->list_metrics});
+    }
+
+    # pass --clear to irrevocably clear the metrics
+    # (necessary when changing existing metric definitions)
+    if (grep { /^--clear$/ } @ARGV) {
+	$jm->{prom}->clear();
+	print "JobMetrics cleared!\n";
+    }
+}
+
 sub new {
     my $class = shift;
-    my $self  = shift || {};
+    my $self  = {};
 
-    $self->{file} = "/tmp/foobik";
+    # TODO: Need to determine best place to store metrics
+    $self->{file} = "/tmp/HTFeed::JobMetrics::Data";
+    if (defined $ENV{'HTFEED_JOBMETRICS_DATA_DIR'}) {
+	$self->{file} = $ENV{'HTFEED_JOBMETRICS_DATA_DIR'};
+    }
     $self->{prom} = Prometheus::Tiny::Shared->new(
 	filename => $self->{file}
     );
@@ -20,36 +69,44 @@ sub new {
     return $self;
 }
 
-sub observe { # for histogram metrics
-    my $self = shift;
+sub observe { # for histograms
+    my $self   = shift;
     my $metric = shift;
-    my $value = shift;
+    my $value  = shift;
 
-    if ($self->valid_metric($metric)) {
-	$self->{prom}->histogram_observe($metric, $value);
-    } else {
-	warn "bad metric $metric\n";
-    }
+    $self->valid_metric($metric) && $self->{prom}->histogram_observe($metric, $value);
 }
 
-sub incr { # for simple metrics
-    my $self = shift;
+sub inc { # for counters
+    my $self   = shift;
     my $metric = shift;
-    my $value = shift;
 
-    if ($self->valid_metric($metric)) {
-	$self->{prom}->incr($metric);
-    } else {
-	warn "bad metric $metric\n";
-    }
+    $self->valid_metric($metric) && $self->{prom}->inc($metric);
 }
 
+# just show names of metrics
+sub list_metrics {
+    my $self = shift;
+    [sort keys %{$self->{metrics}}];
+}
 
+# pretty-printable tabular data
+sub pretty {
+    my $self = shift;
+    $self->{prom}->format
+}
+
+# Check that only valid metrics are used,
+# or make an intelligent complaint.
 sub valid_metric {
-    my $self = shift;
+    my $self   = shift;
     my $metric = shift;
 
-    return defined $self->{metrics}->{$metric};
+    return 1 if defined $self->{metrics}->{$metric};
+
+    my ($package, $filename, $line) = caller(1);
+    warn "invalid metric name \"$metric\" at $filename:$line\n";
+    return 0;
 }
 
 # The leading underscore indicates private
@@ -57,21 +114,39 @@ sub _setup_metrics {
     my $self = shift;
     my $prom = $self->{prom};
 
-    $self->{metrics} = {
-	
-	ms_downloaded => 1,
-	kb_downloaded => 1,
-	ms_remediated => 1,
-	kb_remediated => 1
-    };
+    # Each element x in this array gets turned into 3 prom declarations:
+    # x_ms, x_kb and x_count
+    my @measurable_stages = (
+	'downloaded_dropbox',
+	'downloaded_google',
+	'downloaded_ia',
+	'collated',
+	'fetched',
+	'handled',
+	'packed',
+	'remetsed',
+	'unpacked',
+	'deposited',
+	'sip_deposited',
+	'storage_migrated',
+	'unpacked',
+	'remediated'
+    );
 
-    foreach my $name (keys %{$self->{metrics}}) {
-	my ($type, $stage) = split("_", $name);
-	$prom->declare(
-	    $name,
-	    type => "histogram",
-	    help => "$type processed in stage $stage"
-	);
+    my @scales = qw(items kb ms);
+    # essentially, generate: @measurable_stages x @scales
+    $self->{metrics} = {};
+    foreach my $stage (@measurable_stages) {
+	foreach my $scale (@scales) {
+	    my $name = $stage . "_" . $scale;
+	    # e.g. $self->{metrics}->{deposited_items} = 1
+	    $self->{metrics}->{$name} = 1;
+	    if ($scale eq "items") {
+		$prom->declare($name, type => "counter");
+	    } else {
+		$prom->declare($name, type => "histogram");
+	    }
+	}
     }
 }
 
