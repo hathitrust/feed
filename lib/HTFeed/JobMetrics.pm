56ac9c98fc98855edede1e8f185d72228ff0a7e7
mw <mwarin@umich.edu>
2024-03-27 16:06:12 -0400

    more comments, more commandline options, bucket defs

--- lib/HTFeed/JobMetrics.pm
+++ lib/HTFeed/JobMetrics.pm
@@ -7,7 +7,7 @@ use Prometheus::Tiny::Shared;
 
 # This module is responsible for measuring job performance in different
 # stages. It knows what kinds of things we want to measure, and passes
-# each measurement to a file by prometheus.
+# each measurement to Prometheus, which writes to a Hash::SharedMem.
 
 ### Usage:
 #
@@ -19,6 +19,12 @@ use Prometheus::Tiny::Shared;
 #
 ### irrevocably clear current metrics:
 # $ perl JobMetrics.pm --clear
+#
+### add value to histogram metric:
+# $ perl JobMetrics.pm --observe <metric> <value>
+#
+### increment counter metric:
+# $ perl JobMetrics.pm --inc <metric>
 #
 ### or:
 # use HTFeed::JobMetrics;
@@ -32,23 +38,34 @@ use Prometheus::Tiny::Shared;
 if (!caller) { # If called from commandline:
     my $jm = HTFeed::JobMetrics->new();
 
-    # pass --pretty to see all data pretty-printed
-    if (grep { /^--pretty$/ } @ARGV) {
-	print $jm->pretty . "\n";
+    while(my $arg = shift @ARGV) {
+	if ($arg =~ /^--pretty$/) {
+	    # pass --pretty to see all data pretty-printed
+	    print $jm->pretty . "\n";
+	} elsif ($arg =~ /^--list$/) {
+	    # pass --list to see just the names of known metrics
+	    print join("\n", @{$jm->list_metrics});
+	} elsif ($arg =~ /^--clear$/) {
+	    # pass --clear to irrevocably clear the metrics
+	    # (necessary when changing existing metric definitions)
+	    $jm->clear();
+	    print "JobMetrics cleared!\n";
+	} elsif ($arg =~ /^--observe$/) {
+	    # pass --observe to call observe(metric, value)
+	    # using the next 2 args as metric and value
+	    my $metric = shift @ARGV;
+	    my $value  = shift @ARGV;
+	    $jm->observe($metric, $value);
+	} elsif ($arg =~ /^--inc$/) {
+	    # pass --inc to call inc(metric)
+	    # using the next arg as metric
+	    my $metric = shift @ARGV;
+	    $jm->inc($metric);
+	} else {
+	    print "Ignoring invalid arg: $arg\n";
+	}
     }
-
-    # pass --list to see just the names of known metrics
-    if (grep { /^--list$/ } @ARGV) {
-	print join("\n", @{$jm->list_metrics});
-    }
-
-    # pass --clear to irrevocably clear the metrics
-    # (necessary when changing existing metric definitions)
-    if (grep { /^--clear$/ } @ARGV) {
-	$jm->clear();
-	print "JobMetrics cleared!\n";
-    }
-}
+} # no more commandline
 
 sub new {
     my $class = shift;
@@ -72,6 +89,7 @@ sub new {
 
 # for histograms
 # e.g. $jm->observe("Unpack_ms", 125);
+# or $ perl JobMetrics.pm --observe Unpack_ms 125
 sub observe {
     my $self   = shift;
     my $metric = shift;
@@ -82,6 +100,7 @@ sub observe {
 
 # for counters
 # e.g. $jm->inc("Unpack_items");
+# or $ perl JobMetrics.pm --inc Unpack_items
 sub inc {
     my $self   = shift;
     my $metric = shift;
@@ -107,6 +126,8 @@ sub clear {
     $self->{prom}->clear;
 }
 
+# "private" from here on:
+
 # Check that only valid metrics are used,
 # or make an intelligent complaint.
 # e.g. $jm->_valid_metric("Unpack_ms") # -> 1
@@ -122,7 +143,7 @@ sub _valid_metric { # "private"
     return 0;
 }
 
-sub _setup_metrics { # "private"
+sub _setup_metrics {
     my $self = shift;
     my $prom = $self->{prom};
 
@@ -147,6 +168,13 @@ sub _setup_metrics { # "private"
     my @scales = qw(items kb ms);
     # essentially, generate: @measurable_stages x @scales
     # so that we get Pack_items, Pack_kb, Pack_ms ... etc
+
+    # In case we want different bucket setup for different scales
+    my %scale_buckets = (
+	kb => _kilobyte_buckets(),
+	ms => _millisecond_buckets()
+    );
+
     $self->{metrics} = {};
     foreach my $stage (@measurable_stages) {
 	foreach my $scale (@scales) {
@@ -155,11 +183,51 @@ sub _setup_metrics { # "private"
 	    $self->{metrics}->{$metric_name} = 1;
 	    if ($scale eq "items") {
 		$prom->declare($metric_name, type => "counter");
-	    } else {
-		$prom->declare($metric_name, type => "histogram");
+	    } else { # here, scale is either kb or ms
+		my $buckets = $scale_buckets{$scale};
+		$prom->declare(
+		    $metric_name,
+		    type    => "histogram",
+		    buckets => $buckets
+		);
 	    }
 	}
     }
+}
+
+# NB that if you change these bucket definitions, you need to clear
+# existing data, or prometheus will complain:
+# "redeclaration of <metric_name> with mismatched meta"
+sub _millisecond_buckets {
+    [
+	10, # 10 ms
+	50,
+	100,
+	500,
+	1_000,
+	5_000, # 5 seconds
+	10_000,
+	50_000,
+	100_000,
+	500_000,
+	1000_000,
+	5000_000 # 5000 seconds ~ 1h 23 min
+    ];
+}
+
+sub _kilobyte_buckets {
+    [
+	100, # 100 kb
+	500,
+	1_000,
+	5_000,
+	10_000,
+	50_000, # ~50 MB
+	100_000,
+	500_000,
+	1000_000,
+	5000_000 # ~5 GB
+    ];
 }
 
 1;
