a4b186a86873a711237e4ca533a9079609ffbe18
mw <mwarin@umich.edu>
2024-07-23 17:28:19 -0400

    DEV-1190: Removed unused PackageType classes

--- lib/HTFeed/Namespace/TEST.pm
+++ lib/HTFeed/Namespace/TEST.pm
@@ -8,22 +8,19 @@ use base qw(HTFeed::Namespace);
 our $identifier = 'test';
 
 our $config = {
-    handle_prefix => '2027/deleteme',
-    packagetypes => [qw(ht ia simple kirtas epub simpledigital vendoraudio emma)],
-    description => 'Test namespace',
-    dropbox_folder => '/test-hathitrust-ingest',
-
+    handle_prefix    => '2027/deleteme',
+    packagetypes     => [qw(ht ia simple epub simpledigital emma)],
+    description      => 'Test namespace',
+    dropbox_folder   => '/test-hathitrust-ingest',
     default_timezone => 'America/Detroit',
 };
 
 # Everything is OK except a test invalid barcode
 sub validate_barcode {
-  my $self = shift;
-  my $barcode = shift;
+    my $self    = shift;
+    my $barcode = shift;
 
-  return ($barcode ne 'invalid');
+    return ($barcode ne 'invalid');
 }
 
 1;
-
-__END__
