b2ef8298d6e8e99658763658ff4d736b6b5b1365
mw <mwarin@umich.edu>
2024-08-30 13:29:52 -0400

    improved bin/validate_volume.sh, so you can e.g. debug more easily

--- bin/validate_volume.sh
+++ bin/validate_volume.sh
@@ -1,6 +1,33 @@
-# bash validate_volume.sh /path/to/item.zip
+# In its barest form, test a -p simple -n test volume with:
+# bash validate_volume.sh -z /path/to/item.zip
 
-fullpath=$(realpath $1)
+# Default args:
+clean=""
+zip_path=""
+debug=""
+namespace="test"
+packagetype="simple"
+
+# Override default args:
+while true; do
+    case "$1" in
+        -c | --clean ) clean="--clean";        shift 1 ;;
+        -d | --debug ) debug="-d";             shift 1 ;;
+        -z | --zip_path ) zip_path="$2";       shift 2 ;;
+        -n | --namespace ) namespace="$2";     shift 2 ;;
+        -p | --packagetype ) packagetype="$2"; shift 2 ;;
+        -- ) shift; break ;;
+        * )  break ;;
+    esac
+done
+
+# Check required args:
+if [ -z "$zip_path" ]; then
+    echo "missing required param -z/--zip_path"
+    exit 1
+fi
+
+fullpath=$(realpath $zip_path)
 objid=$(basename $fullpath .zip)
 test_dir=/tmp/prep/toingest/test/
 
@@ -9,4 +36,8 @@ rm    --verbose -rf "$test_dir"
 mkdir --verbose -p  "$test_dir"
 cp    --verbose     "$fullpath" "$test_dir"
 
-perl /usr/local/feed/bin/validate_volume.pl -1 simple test $objid --clean
+# Output the full command & run it
+cmd="perl $debug /usr/local/feed/bin/validate_volume.pl -1 $packagetype $namespace $objid $clean"
+echo "---"
+echo "RUNNING: $cmd"
+eval "$cmd"
